07a32d239c875adfec65f4fb39088090354ca19c
Arnas <arnas.vaiceliunas@gmail.com>
2021-12-21 15:00:30 +0200

    Singleton dp

---
+++ GOF_Creational_Singelton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GOF_Creational_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //can be seen in debug. first time its created, second tame same object is taken.
+            var instance = SingletonInstance.GetInstance();
+            var instance2 = SingletonInstance.GetInstance();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GOF_Creational_Singelton/SingeltonInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_Creational_Singleton
+{
+    public class SingletonInstance
+    {
+        private static SingletonInstance _instance;
+        private SingletonInstance(){}
+
+        public static SingletonInstance GetInstance()
+        {
+            if (_instance == null)
+                _instance = new SingletonInstance();
+
+                return _instance;
+        }
+
+    }
+}
