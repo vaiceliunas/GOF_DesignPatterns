da5dda987677b01d1edaecb094e491d5cef7ee41
Arnas <arnas.vaiceliunas@gmail.com>
2021-12-27 11:58:42 +0200

    Macro command wrapper

---
+++ GOF_Behavioral_Command/Models/Commands/MacroCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GOF_Behavioral_Command.Models.Interfaces;
+
+namespace GOF_Behavioral_Command.Models.Commands
+{
+    public class MacroCommand : ICommand
+    {
+        private ICommand[] _commands;
+
+        public MacroCommand(ICommand[] commands)
+        {
+            _commands = commands;
+        }
+        public void Execute()
+        {
+            for (var i = 0; i < _commands.Length; i++)
+            {
+                _commands[i].Execute();
+            }
+        }
+
+        public void Undo()
+        {
+            Console.WriteLine("Macro command does not support undo");
+        }
+    }
+}
--- GOF_Behavioral_Command/Program.cs
+++ GOF_Behavioral_Command/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using GOF_Behavioral_Command.Models;
 using GOF_Behavioral_Command.Models.Commands;
+using GOF_Behavioral_Command.Models.Interfaces;
 
 namespace GOF_Behavioral_Command
 {
@@ -13,10 +14,14 @@ namespace GOF_Behavioral_Command
             var lightCommand = new LightOnCommand(new Light("kitchen"));
             var lightCommand2 = new LightOnCommand(new Light("living room"));
             var openDoorCommand = new GarageDoorOpenCommand(new GarageDoor());
+
+            var partyOnCommands = new ICommand[]{lightCommand, openDoorCommand};
+            var macroCommandToStartAll = new MacroCommand(partyOnCommands);
             
             remoteControl.SetCommand(lightCommand,0);
             remoteControl.SetCommand(lightCommand2, 1);
             remoteControl.SetCommand(openDoorCommand, 2);
+            remoteControl.SetCommand(macroCommandToStartAll, 3);
             
             remoteControl.ButtonWasPressed(0);
             remoteControl.ButtonWasPressed(1);
@@ -25,6 +30,7 @@ namespace GOF_Behavioral_Command
             remoteControl.UndoWasPressed();
 
             remoteControl.ButtonWasPressed(3);
+            remoteControl.ButtonWasPressed(4);
 
 
 
